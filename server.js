ef04a48ec5f5ebe6b175245819e0cff85a3d073d
unit66 <qrt.unit66@gmail.com>
2019-05-17 12:58:58 +0300

    favicon
    добавил serve-favicon

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const general = require('./routes/general')
+const general = require('./routes/general');
+const path = require('path');
+const favicon = require('serve-favicon');
 
 const app = express();
 const port = process.env.PORT || 666;
 
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use('/', general);
 
 app.listen(port, function() {
