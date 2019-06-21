85741497d7937f93102e5903964987a3591679cd
unit66 <qrt.unit66@gmail.com>
2019-06-21 15:52:13 +0300

    .env.example

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -5,6 +5,7 @@ export default function Home() {
     <section className="home">
       <p>My name is Arseniy. I`m a <span className="teal">JavaScript</span> developer.</p>
       <p>And you are now visiting my pet-project page.</p>
+      <p>You can watch this project repo on <a href="https://github.com/unit66/pet" className="teal" target="_blank">GIT</a>.</p>
     </section>
   );
 }
